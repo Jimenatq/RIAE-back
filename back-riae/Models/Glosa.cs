4de5d01e37b8798cb7422436b3728a66661e230b
Jimenatq <jimena.toledo.quincho@gmail.com>
2022-11-20 23:25:49 -0500

    Agregar archivos de proyecto.

---
+++ back-riae/Models/Glosa.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace back_riae.Models
+{
+    public class Glosa
+    {
+
+        public int Pliego { get; set; }
+
+        public int Subtipo { get; set; }
+
+
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string NombreEmpresa { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string NotaInformativa { get; set; }
+    }
+}
---
+++ back-riae/Models/Ingreso.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace back_riae.Models
+{
+    public class Ingreso
+    {
+        public int Id { get; set; }
+
+        public int Pliego { get; set; }
+
+        public int Tipo { get; set; }
+
+        public int Subtipo { get; set; }
+
+        public int Codigo { get; set; }
+
+        public int Clasificador { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string Concepto { get; set; }
+
+        [Required]
+        public bool Glosa { get; set; }
+
+        public int ImporteT { get; set; }
+    }
+}
